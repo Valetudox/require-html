22dbe30c4078ae0943c270f40fac173c1b9f0a65
Somodi Viktor <somodi.viktor@gmail.com>
2016-04-06 15:27:05 +0200

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var Module = require('module');
+var originalRequire = Module.prototype.require;
+var fs = require('fs');
+
+Module.prototype.require = function(filePath) {
+  if (filePath.indexOf('.html') > -1) {
+    return fs.readFileSync(Module._resolveFilename(filePath, this));
+  }
+  return originalRequire.apply(this, arguments);
+};
