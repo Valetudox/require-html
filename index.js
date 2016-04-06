71504ef61948f867c9c7e19cc2ab8b1f293a843e
Somodi Viktor <somodi.viktor@gmail.com>
2016-04-06 15:37:50 +0200

    fix(index): toString on buffer

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 
 Module.prototype.require = function(filePath) {
   if (filePath.indexOf('.html') > -1) {
-    return fs.readFileSync(Module._resolveFilename(filePath, this));
+    return fs.readFileSync(Module._resolveFilename(filePath, this)).toString();
   }
   return originalRequire.apply(this, arguments);
 };
